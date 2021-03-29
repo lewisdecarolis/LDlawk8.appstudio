5dfe1a5f591cdced3d3146957ff331cb91243b0d
lewisdecarolis <lsd81882@creighton.edu>
2021-03-29 16:03:05 -0500

    first form

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,22 +1,22 @@
 var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
-                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+                  'Keylime Pie','Cherry Cobbler','Chocolate Chip Cookies']
+
 
 dessertVoting.onshow=function(){
+    // always clear before populating
     drpDesserts.clear()   
-  /* add a loop here that adds all the desserts in the array 
-     to the dropdown.
-  */
+    // put array of flavors in the dropdown (called populating it)
+    for (i = 0; i < dessertList.length; i++) 
+        drpDesserts.addItem(dessertList[i])
 }
 
-
-drpDesserts.onclick=function(){
-// this 'if' kicks user out if they  just clicked on control 
-// but not on one item in the list.
-    if (typeof(s) == "object")   
-      return                    
-    else {  // the user picked something
-        /* add code here to get item user chose and output 
-        it in a label, using a literal.
-        */
-    }
+drpDesserts.onclick=function(s) {
+  if (typeof(s) == "object") // means the control was clicked 
+    return                   // but no selection made yet so do nothing
+else if  (s == "") // a selection made
+    ChangeForm(describeYou)  
+    else
+    drpDesserts.value = s     // make dropdown show choice the user made
+    console.log(`The user chose ${s} - that is a great choice.`)
+  
 }
